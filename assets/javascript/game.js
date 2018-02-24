e0d3aa778639bbd3eda22f4de45874181c1c6501
Dan Klinga <dklinga@Dans-MacBook-Pro.local>
2018-02-24 09:56:34 -0500

    saturday

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+// references for DOM elements 
+var $diamond = document.getElementById("button-1");
+var $emerald = document.getElementById("button-2");
+var $ruby = document.getElementById("button-3");
+var $yellow = document.getElementById("button-4");
+var $wins = document.getElementById("wins");
+var $losses = document.getElementById("losses");
+var $number = document.getElementById("randomNumber");
+var $score = document.getElementById("score");
+
+//create vars for game
+var wins = 0;
+var losses = 0;
+var numbersSelected = [];
+
+
+//new game function 
+function newGame {
+//set everything to zero 
+}
+
+//pick a random number for game 
+
+//Pick random numbers for the gems 
