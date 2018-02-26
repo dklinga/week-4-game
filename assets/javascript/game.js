d6073409f7a2dbb95dd7ac1ef9cd4298077d4e80
Dan Klinga <dklinga@Dans-MBP.fios-router.home>
2018-02-25 20:51:30 -0500

    sunday night

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,4 @@
 // references for DOM elements 
-var $diamond = document.getElementById("button-1");
-var $emerald = document.getElementById("button-2");
-var $ruby = document.getElementById("button-3");
-var $yellow = document.getElementById("button-4");
 var $wins = document.getElementById("wins");
 var $losses = document.getElementById("losses");
 var $number = document.getElementById("randomNumber");
@@ -11,14 +7,95 @@ var $score = document.getElementById("score");
 //create vars for game
 var wins = 0;
 var losses = 0;
-var numbersSelected = [];
+var numbersSelected = 0;
+var targetNumber = 0;
 
-
-//new game function 
-function newGame {
-//set everything to zero 
+//new game function set everything to zero
+function newGame() {
+    var numbersSelected = 0;
+    console.log(numbersSelected);
+    $('#score').text(numbersSelected);
+    targetNumber = Math.floor((Math.random() * 100) + 19);
+    $("#randomNumber").text(targetNumber);
+    console.log(targetNumber);
+    var diamond = Math.floor((Math.random() * 12) + 1);
+    console.log(diamond);
+    var emerald = Math.floor((Math.random() * 12) + 1);
+    console.log(emerald);
+    var ruby = Math.floor((Math.random() * 12) + 1);
+    console.log(ruby);
+    var yellow = Math.floor((Math.random() * 12) + 1);
+    console.log(yellow);
 }
 
+
 //pick a random number for game 
+targetNumber = Math.floor((Math.random() * 100) + 19);
+console.log(targetNumber);
+$("#randomNumber").text(targetNumber);
 
 //Pick random numbers for the gems 
+var diamond = Math.floor((Math.random() * 12) + 1);
+console.log(diamond);
+var emerald = Math.floor((Math.random() * 12) + 1);
+console.log(emerald);
+var ruby = Math.floor((Math.random() * 12) + 1);
+console.log(ruby);
+var yellow = Math.floor((Math.random() * 12) + 1);
+console.log(yellow);
+
+//Alert win 
+function win() {
+    alert("Congrats! You won!");
+    wins++;
+    $("#wins").text(wins);
+    newGame();
+}
+
+//Alert lost
+function lost() {
+    alert("Sorry! You lose!");
+    losses++;
+    $('#losses').text(losses);
+    newGame();
+}
+//Button clicks 
+$("#button-1").on("click", function () {
+    numbersSelected = numbersSelected + diamond;
+    $("#score").text(numbersSelected);
+    if (numbersSelected == targetNumber) {
+        win();
+    } else if (numbersSelected > targetNumber) {
+        lost();
+    }
+});
+
+$("#button-2").on("click", function () {
+    numbersSelected = numbersSelected + emerald;
+    $("#score").text(numbersSelected);
+    if (numbersSelected == targetNumber) {
+        win();
+    } else if (numbersSelected > targetNumber) {
+        lost();
+    }
+});
+
+$("#button-3").on("click", function () {
+    numbersSelected = numbersSelected + ruby;
+    $("#score").text(numbersSelected);
+    if (numbersSelected == targetNumber) {
+        win();
+    } else if (numbersSelected > targetNumber) {
+        lost();
+    }
+});
+
+$("#button-4").on("click", function () {
+    numbersSelected = numbersSelected + yellow;
+    $("#score").text(numbersSelected);
+    if (numbersSelected == targetNumber) {
+        win();
+    } else if (numbersSelected > targetNumber) {
+        lost();
+    }
+});
